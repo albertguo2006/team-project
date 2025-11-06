de3c9630b38c5eee12c813b16461784d21ff4189
crrrrrrrrrrrr <cynthiarong2012@gmail.com>
2025-11-06 14:17:44 -0500

    added attributes and get methods to inputdata and outputdata for stock game

--- src/main/java/use_case/stock_game/StockGameOutputData.java
+++ src/main/java/use_case/stock_game/StockGameOutputData.java
@@ -1,4 +1,32 @@
 package use_case.stock_game;
 
+/**
+ * output data for stock game use case
+ **/
+
 public class StockGameOutputData {
+
+    private Double stockPrice;
+    private Double shareNumber;
+    private Double totalEquity;
+
+    // initialise
+    public StockGameOutputData(Double stockPrice, Double shareNumber, Double totalEquity) {
+        this.stockPrice = stockPrice;
+        this.shareNumber = shareNumber;
+        this.totalEquity = totalEquity;
+    }
+
+    public Double getStockPrice() {
+        return stockPrice;
+    }
+
+    public Double getShareNumber() {
+        return shareNumber;
+    }
+
+    public Double getTotalEquity() {
+        return totalEquity;
+    }
+
 }
