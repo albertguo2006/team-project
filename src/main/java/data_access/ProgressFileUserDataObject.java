f53d2fb4cfeeaa0988aa3102ffddb561120e8a1d
amarsjar38 <anjani.sjariffudin@gmail.com>
2025-11-11 16:48:19 -0500

    finished save function for ProgressFileUserDAO.

--- src/main/java/data_access/ProgressFileUserDataObject.java
+++ src/main/java/data_access/ProgressFileUserDataObject.java
@@ -5,13 +5,31 @@ import com.google.gson.JsonObject;
 import entity.Event;
 import entity.NPC;
 import entity.Player;
+import use_case.load_progress.LoadProgressDataAccessInterface;
+import use_case.save_progress.SaveProgressDataAccessInterface;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-public class ProgressFileUserDataObject {
+public class ProgressFileUserDataObject implements SaveProgressDataAccessInterface, LoadProgressDataAccessInterface {
+    private static final String SAVE_FILE = "saveFile.json";
 
-    public void JsonFileWriter(Player player){
+    public void save(Player player) throws IOException {
+        try{
+            FileWriter file = new FileWriter(SAVE_FILE, false);
+            file.write(JsonFileWriter(player).toString());
+            file.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public JsonArray JsonFileWriter(Player player){
         JsonArray saveData = new JsonArray();
 
         Map <NPC, Integer> npcMap = player.getRelationships();
@@ -42,5 +60,17 @@ public class ProgressFileUserDataObject {
         saveData.add(playerData);
         saveData.add(npcData);
         saveData.add(eventData);
+
+        return saveData;
     }
+
+    @Override
+    public void load(Player player) throws IOException {
+        // TODO: finish load function for loading JSON data into entities.
+    }
+
+    public void JsonFileReader(){
+        // TODO: finish JsonFIle Reader function
+    }
+
 }
--- src/main/java/use_case/load_progress/LoadProgressDataAccessInterface.java
+++ src/main/java/use_case/load_progress/LoadProgressDataAccessInterface.java
@@ -2,9 +2,12 @@ package use_case.load_progress;
 
 import entity.Player;
 
+import java.io.IOException;
+
 /**
- * DAO interface for the Save Progress Use Case.
+ * DAO interface for the Load Progress Use Case.
  */
 
 public interface LoadProgressDataAccessInterface {
+    void load(Player player) throws IOException;
 }
