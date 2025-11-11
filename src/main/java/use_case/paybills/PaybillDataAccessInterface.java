5c6d7c3187adf72894cc2cdd6722660211e284ce
14386 <sherrywang782@gmail.com>
2025-11-10 21:55:20 -0500

    small updates

--- src/main/java/use_case/paybills/PaybillDataAccessInterface.java
+++ src/main/java/use_case/paybills/PaybillDataAccessInterface.java
@@ -1,4 +1,18 @@
 package use_case.paybills;
 
-public class PaybillDataAccessInterface {
+import entity.Bill;
+
+import java.util.List;
+
+public interface PaybillDataAccessInterface {
+
+    /**
+     * Returns the bills not yet paid by the player
+     */
+    List<Bill> getUnpaidBills();
+    /**
+     * Returns all the bills the player has
+     */
+    List<Bill> getALLBills();
+
 }
