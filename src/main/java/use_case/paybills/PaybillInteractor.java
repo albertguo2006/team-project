6bc681d5a50f0802f62186ef9aa20687c9374b21
14386 <sherrywang782@gmail.com>
2025-11-12 16:58:13 -0500

    Modified a bit of every file

--- src/main/java/use_case/paybills/PaybillInteractor.java
+++ src/main/java/use_case/paybills/PaybillInteractor.java
@@ -1,4 +1,19 @@
 package use_case.paybills;
 
-public class PaybillInteractor {
+public class PaybillInteractor implements PaybillInputBoundary {
+    private final PaybillDataAccessInterface paybillDataAccess;
+    private final PaybillOutputBoundary paybillPresenter;
+
+    public PaybillInteractor(PaybillDataAccessInterface paybillDataAccess, PaybillOutputBoundary paybillPresenter) {
+        this.paybillDataAccess = paybillDataAccess;
+        this.paybillPresenter = paybillPresenter;
+    }
+
+    @Override
+    public void execute() {
+        // TODO: implement logic of paybill use case
+        //* set respective bill to paid and update player balance
+        //* instantiate the 'paybillOutputData' which needs to contain everything
+        //* tell the presenter to prepare a success view
+    }
 }
--- src/main/java/use_case/paybills/PaybillOutputData.java
+++ src/main/java/use_case/paybills/PaybillOutputData.java
@@ -1,4 +1,23 @@
 package use_case.paybills;
 
+import entity.Bill;
+
+import java.util.List;
+
 public class PaybillOutputData {
+    private final List<Bill> billList;
+    private final boolean useCaseFailed;
+
+    public PaybillOutputData(List<Bill> billList, boolean useCaseFailed) {
+        this.billList = billList;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public List<Bill> getBillList() {
+        return billList;
+    }
+
+    public boolean isUseCaseFailed() {
+        return useCaseFailed;
+    }
 }
