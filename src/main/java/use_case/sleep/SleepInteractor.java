3eb55192ac58b245da8891e45874b1a7344eced9
14386 <sherrywang782@gmail.com>
2025-11-17 16:10:21 -0500

    Started on sleep

--- src/main/java/use_case/sleep/SleepInteractor.java
+++ src/main/java/use_case/sleep/SleepInteractor.java
@@ -1,4 +1,30 @@
 package use_case.sleep;
 
+import entity.Player;
+import use_case.save_progress.SaveProgressInteractor;
+
+/**
+ * the Sleep Interactor
+ */
+
 public class SleepInteractor {
+
+    private final Player player;
+    private final SaveProgressInteractor saveProgressInteractor;
+    private int currentDay;
+
+    public SleepInteractor(Player player, SaveProgressInteractor saveProgressInteractor){
+        this.player = player;
+        this.saveProgressInteractor = saveProgressInteractor;
+        this.currentDay = 1;
+    }
+
+    /**
+     * Execute the sleep sequence: save game and advance to next day
+     * @throws IllegalStateException if player is not in house
+     */
+    public void execute(){
+        if (!player.isInHouse())
+    }
+
 }
