58b520d8024c6378064a773a9d8d6cfa218f377a
jiaboni <joshuaiaboni06@gmail.com>
2025-11-22 19:37:26 -0500

    Added test cases for ActivateRandomOutcomeInteractor and StartRandomEventInteractor

---
+++ src/test/java/Events/StartRandomEventInteractorTest.java
@@ -0,0 +1,35 @@
+package Events;
+
+import data_access.EventDataAccessObject;
+import entity.Event;
+import entity.Player;
+import org.junit.Test;
+import use_case.events.ActivateRandomOutcome.*;
+import use_case.events.StartRandomEvent.StartRandomEventInputBoundary;
+import use_case.events.StartRandomEvent.StartRandomEventInteractor;
+import use_case.events.StartRandomEvent.StartRandomEventOutputBoundary;
+import use_case.events.StartRandomEvent.StartRandomEventOutputData;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StartRandomEventInteractorTest {
+
+    @Test
+    public void successStartRandomEventTest() {
+        EventDataAccessObject eventDataAccessObject = new EventDataAccessObject();
+        eventDataAccessObject.createEventList();
+
+        // Success Presenter
+        StartRandomEventOutputBoundary startRandomEventPresenter = new StartRandomEventOutputBoundary() {
+            @Override
+            public void prepareSuccessView(StartRandomEventOutputData startRandomEventOutputData) {
+                // Test that the random event chosen was one of the events in the DAO
+                assertTrue(eventDataAccessObject.getEventList().contains(startRandomEventOutputData.getEvent()));
+            }
+        };
+
+        // Manually set eventPity to guarentee an event is chosen
+        StartRandomEventInputBoundary startRandomEventInteractor = new StartRandomEventInteractor(eventDataAccessObject,
+                startRandomEventPresenter, 200);
+        startRandomEventInteractor.execute();
+    }
+}
