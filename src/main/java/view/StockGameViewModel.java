7c7a5dd43fe0e42ef7471bea355dc4dda1d3f8d2
albertguo2006 <albertguo2006@gmail.com>
2025-11-24 13:41:01 -0500

    stock trading gui

--- src/main/java/view/StockGameViewModel.java
+++ src/main/java/view/StockGameViewModel.java
@@ -2,7 +2,12 @@ package view;
 
 import use_case.stock_game.play_stock_game.PlayStockGameOutputData;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class StockGameViewModel {
+
+    public static final int MAX_HISTORY_SIZE = 50;
 
     public double price;
     public double cash;
@@ -11,10 +16,19 @@ public class StockGameViewModel {
 
     public double lastPrice; // to calculate next price and present green/red arrow
 
+    public Queue<Double> priceHistory = new LinkedList<>();
+
     public void update(PlayStockGameOutputData data) {
         this.price = data.price;
         this.cash = data.cash;
         this.shares = data.shares;
         this.equity = data.totalEquity;
+    }
+
+    public void addPriceToHistory(double price) {
+        priceHistory.add(price);
+        if (priceHistory.size() > MAX_HISTORY_SIZE) {
+            priceHistory.poll(); // Remove oldest price
+        }
     }
 }
