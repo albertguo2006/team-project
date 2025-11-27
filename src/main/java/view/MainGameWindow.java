6a1fad44e7fa3f899e3cc9392f60cd75372c7da4
albertguo2006 <albertguo2006@gmail.com>
2025-11-27 09:15:39 -0500

    drop items can now be picked up again

--- src/main/java/view/MainGameWindow.java
+++ src/main/java/view/MainGameWindow.java
@@ -421,8 +421,16 @@ public class MainGameWindow extends JFrame {
             int slotKey = slotIndex + 1;  // Convert 0-4 to 1-5
             Item item = player.getInventory().get(slotKey);
             if (item != null) {
+                // Get player's current position and zone
+                double dropX = player.getX();
+                double dropY = player.getY();
+                String currentZone = gamePanel.getGameMap().getCurrentZone().getName();
+
+                // Remove from inventory and place in world
                 player.removeInventory(slotKey);
-                System.out.println("Dropped " + item.getName());
+                worldItemDataAccess.addDroppedItem(item, currentZone, dropX, dropY);
+
+                System.out.println("Dropped " + item.getName() + " at (" + dropX + ", " + dropY + ") in " + currentZone);
                 gamePanel.setSelectedInventorySlot(-1);  // Deselect after drop
             }
         });
